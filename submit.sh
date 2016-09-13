835c5295da3153d229ac55d335ae720ae39c509f
tkhunkhe <tkhunkhe@gmail.com>
2016-09-13 13:24:02 -0400

    update .rmd and docs/index.html files

--- submit.sh
+++ submit.sh
@@ -2,8 +2,7 @@
 
 mv asmt1.html docs/
 mv docs/asmt1.html docs/index.html
-git add docs/
-git add asmt1.rmd
+git add .
 git commit -m "update .rmd and docs/index.html files"
 git push
 echo "done updating (and pushing)"
